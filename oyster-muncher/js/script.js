3d81ae77f864c3120960e5ffdd40f39fe5d9d079
KV-00 <vaillancourtkeven@gmail.com>
2024-02-25 22:34:25 -0500

    initial commit for OYSTER MUNCHER

---
+++ oyster-muncher/js/script.js
@@ -0,0 +1,27 @@
+/**
+OYSTER MUNCHER
+By: Keven Vaillancourt
+
+If you are reading this, this project is currently unfinished and you're seeing code I'm sneakily trying to complete before anyone marks it!
+*/
+
+"use strict";
+
+function preload() {
+
+}
+
+
+function setup() {
+    createCanvas(displayWidth, displayHeight);
+
+    textSize(50);
+    textAlign(CENTER);
+    textWrap(WORD);
+    text('If you are reading this, this project is currently unfinished and you are seeing a webpage I am sneakily trying to complete before anyone marks it! ;)', width/6, height/2.5, 1000);
+}
+
+
+function draw() {
+
+}
